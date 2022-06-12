60da339cabb3df9f2e12f8a4c5d5b2f6a45943aa
zhangtao-cpu <1305381285@qq.com>
2022-06-12 23:28:00 +0800

    最新更新1

--- rpc-framework-simple/src/main/java/github/javaguide/registry/zk/util/CuratorUtils.java
+++ rpc-framework-simple/src/main/java/github/javaguide/registry/zk/util/CuratorUtils.java
@@ -35,7 +35,7 @@ public final class CuratorUtils {
     private static final Map<String, List<String>> SERVICE_ADDRESS_MAP = new ConcurrentHashMap<>();
     private static final Set<String> REGISTERED_PATH_SET = ConcurrentHashMap.newKeySet();
     private static CuratorFramework zkClient;
-    private static final String DEFAULT_ZOOKEEPER_ADDRESS = "127.0.0.1:2181";
+    private static final String DEFAULT_ZOOKEEPER_ADDRESS = "139.224.22.217:2181";
 
     private CuratorUtils() {
     }
